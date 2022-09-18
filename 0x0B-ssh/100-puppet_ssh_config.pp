b3f1b50108093dc0882826ebc6160ef4be641c39
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-18 08:02:47 +0300

    4.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,14 @@
-#!/usr/bin/env bash
+# Client config file
+include stdlib
 
+file_line {
+  'password_auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    PasswordAuthentication no'
+  ;
+  'key_location':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    IdentityFile ~/.ssh/holberton'
+}
