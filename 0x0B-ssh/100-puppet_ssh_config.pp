0700bbdfb4d25c7cb5c139902b064787618be8a6
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-09-18 08:31:44 +0300

    4. # adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
 # Client config file
 include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
 
-file_line {
-  'password_auth':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    PasswordAuthentication no'
-  ;
-  'key_location':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    IdentityFile ~/.ssh/holberton'
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
