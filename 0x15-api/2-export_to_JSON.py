043480e5f13ab259477d807b242b681ee2ce7333
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-18 05:36:53 +0300

    err

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,8 +7,18 @@ import requests
 
 
 if __name__ == "__main__":
-    resp = (requests.get('https://jsonplaceholder.typicode.com/todos', params={"userId": argv[1]})).json()
-    user = (requests.get('https://jsonplaceholder.typicode.com/users/{}'. format(argv[1]))).json()
+    resp = (
+        requests.get(
+            'https://jsonplaceholder.typicode.com/todos',
+            params={"userId": argv[1]}
+        )
+    ).json()
+    user = (
+        requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            . format(argv[1])
+        )
+    ).json()
     with open(f"{argv[1]}.json", "w") as jf:
         json.dump(
             {
