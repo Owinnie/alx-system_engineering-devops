943c6f48fada5b55506104031fbe58ddf45c75a3
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-18 03:02:41 +0300

    to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from sys import argv
+import csv
+import json
+import requests
+
+
+if __name__ == "__main__":
+    resp = (requests.get('https://jsonplaceholder.typicode.com/todos', params={"userId": argv[1]})).json()
+    user = (requests.get('https://jsonplaceholder.typicode.com/users/{}'. format(argv[1]))).json()
+    with open(f"{argv[1]}.json", "w") as jf:
+        json.dump(
+            {
+                argv[1]: [
+                    {
+                        "task": i.get('title'),
+                        "completed": i.get('completed'),
+                        "username": user.get('username')
+                    }
+                    for i in resp
+                ]
+            },
+            jf
+        )
