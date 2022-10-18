ba7c79cfffb6905f25e6493cd2d2a6e68bc94f31
Owinnie <tillawinnie@gmail.com>
2022-10-17 17:19:34 -0700

    PEP

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,8 +7,23 @@ import requests
 
 
 if __name__ == "__main__":
-    resp = (requests.get('https://jsonplaceholder.typicode.com/todos', params={"userId": argv[1]})).json()
-    user = (requests.get('https://jsonplaceholder.typicode.com/users/{}'. format(argv[1]))).json()
-    done = [i.get('title') for i in resp if i.get('completed') == True]
-    print(f"Employee {user.get('name')} is done with tasks {len(done)}/{len(resp)}")
+    resp = (
+        requests
+        .get(
+            'https://jsonplaceholder.typicode.com/todos',
+            params={"userId": argv[1]}
+            )
+        ).json()
+    user = (
+        requests
+        .get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            . format(argv[1])
+            )
+        ).json()
+    done = [i.get('title') for i in resp if i.get('completed') is True]
+    print(
+        f"Employee {user.get('name')}
+        is done with tasks {len(done)}/{len(resp)}"
+        )
     [print(f"\t {tsk}") for tsk in done]
