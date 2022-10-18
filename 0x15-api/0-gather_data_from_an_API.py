53b3eb89d6858b744c9e0cadc3ab2f8aa6fd3990
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-10-18 05:08:45 +0300

    Errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,11 @@ import sys
 
 
 if __name__ == "__main__":
-    resp = (
-        requests
-        .get(
-            'https://jsonplaceholder.typicode.com/todos',
-            params={"userId": sys.argv[1]}
-            )
-        ).json()
-    user = (
-        requests
-        .get(
-            'https://jsonplaceholder.typicode.com/users/{}'
-            . format(sys.argv[1])
-            )
-        ).json()
-    done = [i.get('title') for i in resp if i.get('completed') is True]
-    print(
-        "Employee {} is done with tasks {}/{}"
-        .format(user.get('name'), len(done), len(resp)))
-    [print("\t {}".format(tsk)) for tsk in done]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
