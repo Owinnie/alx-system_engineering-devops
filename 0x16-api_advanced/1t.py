fef366ecb206cb527952dd128ad85438e2f2881d
Owinnie <tillawinnie@gmail.com>
2022-11-01 14:25:31 -0700

    top ten drama

---
+++ 0x16-api_advanced/1t.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 1. Top Ten """
+
+import requests as rq
+
+
+def top_ten(subreddit):
+    """ Print titles of top 10 hot posts """
+    if subreddit:
+        res = rq.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+                     v1.0.0 (by /u/aaorrico23)'},
+                     params={'limit': 10}).json()
+        pos_10 = res.get('data', {}).get('children', None)
+        if pos_10 is None or (len(pos_10) > 0 and pos_10[0].get('kind') != 't3'):
+            print(None)
+        else:
+            for post in pos_10:
+                print(post.get('data', {}).get('title', None))
+    # print(None)
