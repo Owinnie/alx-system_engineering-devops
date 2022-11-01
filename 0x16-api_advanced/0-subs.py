e99e73dfaa63d83b03e81dc90970b612aaf892f8
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-11-01 23:06:43 +0300

    0 subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,10 @@ def number_of_subscribers(subreddit):
         resp = requests.get('http://www.reddit.com/r/{}/about.json'
                             .format(subreddit),
                             headers={
-                                'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/aaorrico23)'
+                                'User-Agent': 'Python/requests:APIproject:
+                                v1.0.0 (by /u/aaorrico23)'
                             }
-                        ).json()
+               ).json()
         sub = resp.get("data", {}).get("subscribers", 0)
         return sub
     return 0
