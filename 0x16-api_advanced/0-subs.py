11a7c1544bc528cf7977c9f7c99f266bb0a50ebc
Owinnie <tillawinnie@gmail.com>
2022-11-01 13:17:15 -0700

    pep error correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """ 0. How many subs? """
 
-import requests
+import requests as rq
 
 
 def number_of_subscribers(subreddit):
     """ Query reddit API & return no. of subs """
     if subreddit:
-        resp = requests.get('http://www.reddit.com/r/{}/about.json'
-                            .format(subreddit),
-                            headers={
-                                'User-Agent': 'Python/requests:APIproject:
-                                v1.0.0 (by /u/aaorrico23)'
-                            }
-               ).json()
+        res = rq.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
         sub = resp.get("data", {}).get("subscribers", 0)
         return sub
     return 0
